184df32fdd8bc62bd36406fb823923cfeff34a37
dylanwithgravity <dylanwithgravity@gmail.com>
2018-08-04 17:38:17 -0400

    Added retrieval of image recognition results from V3 model

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -29,13 +29,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
-        if let userPhoto = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        if let userSelectedPhoto = info[UIImagePickerControllerOriginalImage] as? UIImage {
             
-            imageView.image = userPhoto
-
+            imageView.image = userSelectedPhoto
+            
+            // if ciimage is nil, trigger else statement that gives error message
+            guard let ciimage = CIImage(image: userSelectedPhoto) else {
+                fatalError("Could not convert to a CIImage")
+            }
+            
+            detect(image: ciimage)
+       
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        // if model is nil, trigger else statement that gives error message
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Error, model is nil")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+          try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
         
     }
     
