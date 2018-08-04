28435eed3607621e2b11b9a2e66a72b78ca91c19
dylanwithgravity <dylanwithgravity@gmail.com>
2018-08-04 17:49:22 -0400

    Finished simple app using CoreML, detects if the selected photo is a hotdog or not!(inspired by Silicon Valley)

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -58,7 +58,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 fatalError("Model failed to process image")
             }
             
-            print(results)
+            //check first result from model
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+            
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
